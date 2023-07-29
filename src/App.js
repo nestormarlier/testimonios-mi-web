3394cbe640cf0af719e6cc6cca8b3f4a35ef2c06
nestormarlier <nestor,margmail.com>
2023-07-29 15:34:37 -0300

    Uso de Props para el pasaje de datos

--- src/App.js
+++ src/App.js
@@ -6,7 +6,27 @@ function App() {
     <div className="App">
       <div className='contenedor-principal'>
         <h1>Esto es lo que dicen nuestros alumnos sobre freeCodeCamp:</h1>
-        <Testimonio />
+        <Testimonio 
+          imagen='shawn'
+          nombre='Shawn Wang'
+          pais='Singapur'
+          cargo='Ingeniero de software'
+          empresa='Amazon'
+          testimonio="Da miedo cambiar de carrera. Solo gané confianza en que podía codificar trabajando a través de los cientos de horas de lecciones gratuitas en freeCodeCamp. En un año tenía un trabajo de seis cifras como ingeniero de software. freeCodeCamp cambió mi vida." />
+        <Testimonio 
+          imagen='sarah'
+          nombre='Sarah Chima'
+          pais='Nigeria'
+          cargo='Ingeniero de software'
+          empresa='ChatDesk'
+          testimonio='freeCodeCamp fue la puerta de entrada a mi carrera como desarrollador de software. El plan de estudios bien estructurado llevó mi conocimiento de codificación de un nivel de principiante total a un nivel muy seguro. Era todo lo que necesitaba para conseguir mi primer trabajo de desarrollador en una empresa increíble' />
+        <Testimonio 
+          imagen='emma'
+          nombre='Emma Bostian'
+          pais='Suecia'
+          cargo='Ingeniero de software'
+          empresa='Spotify'
+          testimonio='Siempre he tenido problemas para aprender JavaScript. Tomé muchos cursos, pero el curso de freeCodeCamp fue el que se quedó. Estudiar JavaScript, así como estructuras de datos y algoritmos en freeCodeCamp me dio las habilidades y la confianza que necesitaba para conseguir el trabajo de mis sueños como un ingeniero de software en Spotify' />
       </div>
     </div>
   );
--- src/componentes/Testimonio.js
+++ src/componentes/Testimonio.js
@@ -1,17 +1,17 @@
 import React from "react";
 import '../hojas-de-estilo-stylesheets/Testimonio.css';
 
-function Testimonio(){
+function Testimonio(props){
 	return (
 		<div className="contenedor-testimonio">
 			<img
 				className="imagenes-testimonio"
-				src={require("../imagenes/testimonio-emma.png")}
-				alt="Foto de Emma" /> 
+				src={require(`../imagenes/testimonio-${props.imagen}.png`)}
+				alt= {'Foto de '+props.nombre} /> 
 			<div className="contenedor-texto-testimonio">
-				<p className="nombre-testimonio">Emma Bostian en Suecia</p>
-				<p className="cargo-testimonio">Ingeniera de software en Spotify</p>
-				<p className="texto-testimonio">"Siempre he tenido problemas para aprender JavaScript. He tomado muchos cursos, pero el curso de freeCodeCamp fue el que se quedó. Estudiar JavaScript, así como estructuras de datos y algoritmos en freeCodeCamp me dio las habilidades y la confianza que necesitaba para conseguir el trabajo de mis sueños como ingeniero de software en Spotify".</p>
+				<p className="nombre-testimonio"><strong>{props.nombre}</strong> en {props.pais}</p>
+				<p className="cargo-testimonio">{props.cargo} en <strong>{props.empresa}</strong></p>
+				<p className="texto-testimonio">{props.testimonio}</p>
 			</div>
 	 	</div>
 	);
